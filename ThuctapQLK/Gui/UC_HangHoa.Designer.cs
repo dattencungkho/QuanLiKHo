2e8a34ed537b6a508d8fdc29cd8c035caeb46d40
BuiDien96 <dienbui90@gmail.com>
2017-06-04 22:34:25 +0700

    add hang hoa

---
+++ ThuctapQLK/Gui/UC_HangHoa.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace ThuctapQLK.Gui
+{
+    partial class UC_HangHoa
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnluu = new System.Windows.Forms.Button();
+            this.btnxoa = new System.Windows.Forms.Button();
+            this.btnsua = new System.Windows.Forms.Button();
+            this.btnthem = new System.Windows.Forms.Button();
+            this.txtnhaCC = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txttenHH = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtmaHH = new System.Windows.Forms.TextBox();
+            this.lbl_mahh = new System.Windows.Forms.Label();
+            this.dgvhangHoa = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtsoLuong = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvhangHoa)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnluu
+            // 
+            this.btnluu.BackColor = System.Drawing.SystemColors.ControlText;
+            this.btnluu.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnluu.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnluu.Location = new System.Drawing.Point(556, 148);
+            this.btnluu.Name = "btnluu";
+            this.btnluu.Size = new System.Drawing.Size(70, 33);
+            this.btnluu.TabIndex = 52;
+            this.btnluu.Text = "Lưu";
+            this.btnluu.UseVisualStyleBackColor = false;
+            this.btnluu.Click += new System.EventHandler(this.btnluu_Click);
+            // 
+            // btnxoa
+            // 
+            this.btnxoa.BackColor = System.Drawing.SystemColors.ControlText;
+            this.btnxoa.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnxoa.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnxoa.Location = new System.Drawing.Point(480, 148);
+            this.btnxoa.Name = "btnxoa";
+            this.btnxoa.Size = new System.Drawing.Size(70, 33);
+            this.btnxoa.TabIndex = 53;
+            this.btnxoa.Text = "Xóa";
+            this.btnxoa.UseVisualStyleBackColor = false;
+            this.btnxoa.Click += new System.EventHandler(this.btnxoa_Click);
+            // 
+            // btnsua
+            // 
+            this.btnsua.BackColor = System.Drawing.SystemColors.ControlText;
+            this.btnsua.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsua.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnsua.Location = new System.Drawing.Point(555, 107);
+            this.btnsua.Name = "btnsua";
+            this.btnsua.Size = new System.Drawing.Size(70, 33);
+            this.btnsua.TabIndex = 54;
+            this.btnsua.Text = "Sửa";
+            this.btnsua.UseVisualStyleBackColor = false;
+            this.btnsua.Click += new System.EventHandler(this.btnsua_Click);
+            // 
+            // btnthem
+            // 
+            this.btnthem.BackColor = System.Drawing.SystemColors.ControlText;
+            this.btnthem.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnthem.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnthem.Location = new System.Drawing.Point(480, 107);
+            this.btnthem.Name = "btnthem";
+            this.btnthem.Size = new System.Drawing.Size(70, 33);
+            this.btnthem.TabIndex = 55;
+            this.btnthem.Text = "Thêm";
+            this.btnthem.UseVisualStyleBackColor = false;
+            this.btnthem.Click += new System.EventHandler(this.btnthem_Click);
+            // 
+            // txtnhaCC
+            // 
+            this.txtnhaCC.Location = new System.Drawing.Point(325, 107);
+            this.txtnhaCC.Margin = new System.Windows.Forms.Padding(2);
+            this.txtnhaCC.Name = "txtnhaCC";
+            this.txtnhaCC.Size = new System.Drawing.Size(138, 20);
+            this.txtnhaCC.TabIndex = 51;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label5.Location = new System.Drawing.Point(246, 107);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 16);
+            this.label5.TabIndex = 50;
+            this.label5.Text = "Nhà CC";
+            // 
+            // txttenHH
+            // 
+            this.txttenHH.Location = new System.Drawing.Point(71, 147);
+            this.txttenHH.Margin = new System.Windows.Forms.Padding(2);
+            this.txttenHH.Name = "txttenHH";
+            this.txttenHH.Size = new System.Drawing.Size(143, 20);
+            this.txttenHH.TabIndex = 49;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(6, 148);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 16);
+            this.label1.TabIndex = 48;
+            this.label1.Text = "Tên HH";
+            // 
+            // txtmaHH
+            // 
+            this.txtmaHH.Location = new System.Drawing.Point(71, 107);
+            this.txtmaHH.Margin = new System.Windows.Forms.Padding(2);
+            this.txtmaHH.Name = "txtmaHH";
+            this.txtmaHH.Size = new System.Drawing.Size(143, 20);
+            this.txtmaHH.TabIndex = 47;
+            // 
+            // lbl_mahh
+            // 
+            this.lbl_mahh.AutoSize = true;
+            this.lbl_mahh.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_mahh.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lbl_mahh.Location = new System.Drawing.Point(6, 107);
+            this.lbl_mahh.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_mahh.Name = "lbl_mahh";
+            this.lbl_mahh.Size = new System.Drawing.Size(55, 16);
+            this.lbl_mahh.TabIndex = 46;
+            this.lbl_mahh.Text = "Mã HH";
+            // 
+            // dgvhangHoa
+            // 
+            this.dgvhangHoa.AllowUserToAddRows = false;
+            this.dgvhangHoa.AllowUserToDeleteRows = false;
+            this.dgvhangHoa.AllowUserToResizeRows = false;
+            this.dgvhangHoa.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvhangHoa.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dgvhangHoa.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvhangHoa.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dgvhangHoa.Location = new System.Drawing.Point(0, 253);
+            this.dgvhangHoa.Margin = new System.Windows.Forms.Padding(2);
+            this.dgvhangHoa.Name = "dgvhangHoa";
+            this.dgvhangHoa.RowTemplate.Height = 24;
+            this.dgvhangHoa.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvhangHoa.Size = new System.Drawing.Size(625, 204);
+            this.dgvhangHoa.TabIndex = 45;
+            this.dgvhangHoa.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvhangHoa_CellContentClick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(625, 89);
+            this.panel1.TabIndex = 56;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(228, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(191, 36);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "HÀNG HÓA";
+            // 
+            // txtsoLuong
+            // 
+            this.txtsoLuong.Location = new System.Drawing.Point(325, 151);
+            this.txtsoLuong.Margin = new System.Windows.Forms.Padding(2);
+            this.txtsoLuong.Name = "txtsoLuong";
+            this.txtsoLuong.Size = new System.Drawing.Size(138, 20);
+            this.txtsoLuong.TabIndex = 58;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(246, 151);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 16);
+            this.label3.TabIndex = 57;
+            this.label3.Text = "Số lượng";
+            // 
+            // UC_HangHoa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Controls.Add(this.txtsoLuong);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnluu);
+            this.Controls.Add(this.btnxoa);
+            this.Controls.Add(this.btnsua);
+            this.Controls.Add(this.btnthem);
+            this.Controls.Add(this.txtnhaCC);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txttenHH);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtmaHH);
+            this.Controls.Add(this.lbl_mahh);
+            this.Controls.Add(this.dgvhangHoa);
+            this.Name = "UC_HangHoa";
+            this.Size = new System.Drawing.Size(625, 457);
+            this.Load += new System.EventHandler(this.HangHoa_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvhangHoa)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnluu;
+        private System.Windows.Forms.Button btnxoa;
+        private System.Windows.Forms.Button btnsua;
+        private System.Windows.Forms.Button btnthem;
+        private System.Windows.Forms.TextBox txtnhaCC;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txttenHH;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtmaHH;
+        private System.Windows.Forms.Label lbl_mahh;
+        private System.Windows.Forms.DataGridView dgvhangHoa;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtsoLuong;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ ThuctapQLK/Gui/UC_HangHoa.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ThuctapQLK.DT0;
+using ThuctapQLK.BUS;
+namespace ThuctapQLK.Gui
+{
+    public partial class UC_HangHoa : UserControl
+    {
+        public UC_HangHoa()
+        {
+            InitializeComponent();
+        }
+        private void xuat()
+        {
+            dgvhangHoa.DataSource = BUS.BUS.xuat_hh();
+        }
+        private void HangHoa_Load(object sender, EventArgs e)
+        {
+            xuat();
+        }
+        private void dgvhangHoa_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtmaHH.Text = dgvhangHoa.CurrentRow.Cells[0].Value.ToString();
+            txttenHH.Text = dgvhangHoa.CurrentRow.Cells[1].Value.ToString();
+            txtnhaCC.Text = dgvhangHoa.CurrentRow.Cells[2].Value.ToString();
+         //   dgvhangHoa.DataSource = BUS.BUS.xuat_ctn(txtsoLuong.Text);
+         //  txtsoLuong.Text = dgvhangHoa.CurrentRow.Cells[3].Value.ToString();
+           //.DataSource = BUS.BUS.xuat_ctn(txtsoLuong.Text);
+        }
+
+        private void btnthem_Click(object sender, EventArgs e)
+        {
+            txtmaHH.Text = string.Empty;
+            txttenHH.Text = string.Empty;
+
+            txtnhaCC.Text = string.Empty;
+            txtsoLuong.Text = string.Empty;
+            
+
+        }
+
+        private void btnluu_Click(object sender, EventArgs e)
+        {
+            if (txtmaHH.Text == " " || txttenHH.Text == " ")
+            {
+                MessageBox.Show("Hihi");
+                return;
+            }
+            HangHoa a = new HangHoa();
+            a.MaHH = txtmaHH.Text;
+            a.TenHH = txttenHH.Text;
+            a.MaNCC = txtnhaCC.Text;
+            a.SoLuong = txtsoLuong.Text;
+
+            if (MessageBox.Show(string.Format("Ban co muon them ncc {0} tên ncc {1}", a.MaHH, a.TenHH), "", MessageBoxButtons.OKCancel) == DialogResult.OK)
+            {
+
+                if (BUS.BUS.them_HH(a) > 0)
+                {
+                    MessageBox.Show("Them thanh cong");
+                    HangHoa_Load(sender, e);
+                }
+                else
+                { MessageBox.Show("Loi"); }
+            }
+        }
+
+        private void btnsua_Click(object sender, EventArgs e)
+        {
+            if (txtmaHH.Text == "" || txttenHH.Text == "")
+            {
+                MessageBox.Show("Chưa nhập đủ thông tin!");
+                return;
+            }
+            HangHoa a = new HangHoa();
+            a.MaHH = txtmaHH.Text;
+            a.TenHH = txttenHH.Text;
+            a.MaNCC = txtnhaCC.Text;
+            a.SoLuong = txtsoLuong.Text;
+            
+
+            if (MessageBox.Show(string.Format("Bạn có muốn sửa ma hh {0} ", a.MaHH),
+                "", MessageBoxButtons.OKCancel) == DialogResult.OK)
+            {
+                if (BUS.BUS.sua_HH(a) > 0)
+                {
+                    MessageBox.Show("Sửa thành công!");
+                    HangHoa_Load(sender, e);
+                }
+                else
+                    MessageBox.Show("Lỗi");
+            }
+
+            
+        }
+
+        private void btnxoa_Click(object sender, EventArgs e)
+        {
+            if (txtmaHH.Text == "" || txttenHH.Text == "")
+            {
+                MessageBox.Show("Chưa nhập đủ thông tin!");
+                return;
+            }
+            HangHoa a = new HangHoa();
+            a.MaHH = txtmaHH.Text;
+            a.TenHH = txttenHH.Text;
+            a.MaNCC = txtnhaCC.Text;
+            a.SoLuong = txtsoLuong.Text;
+
+            if (MessageBox.Show(string.Format("Bạn có muốn xóa vị trí mã {0} ", a.MaNCC),
+                                "", MessageBoxButtons.OKCancel) == DialogResult.OK)
+            {
+                if (BUS.BUS.xoa_HH(a) > 0)
+                {
+                    MessageBox.Show("Xóa thành công!");
+                    HangHoa_Load(sender, e);
+                }
+                else
+                    MessageBox.Show("Lỗi");
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
